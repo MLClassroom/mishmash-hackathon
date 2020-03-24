fc55ccc1a7195b5776602774fa4a3783d6b4501f
Nishanth Koganti <buntyke@gmail.com>
2020-03-24 23:21:45 +0530

    update pytest configuration

--- tests/test_initws.py
+++ tests/test_initws.py
@@ -9,7 +9,8 @@ import pytest
 from aml_scripts.utils import set_env_vars, get_svc_pr, get_ws
 
 
-@pytest.mark.parametrize('config_path', ['src/aml_scripts/.aml_config/config.json'])
+@pytest.mark.local
+@pytest.mark.parametrize('config_path', ['aml_scripts/.aml_config/config.json'])
 def test_env_vars(config_path):
     # set env variables
     status = set_env_vars(config_path)
@@ -18,11 +19,9 @@ def test_env_vars(config_path):
     assert status
 
 
-@pytest.mark.parametrize('config_path', ['src/aml_scripts/.aml_config/config.json'])
-def test_authentication(config_path):
-    # set env variables
-    set_env_vars(config_path)
-
+@pytest.mark.cicd
+@pytest.mark.local
+def test_authentication():
     # perform service principal authentication
     svc_pr = get_svc_pr()
 
@@ -30,11 +29,9 @@ def test_authentication(config_path):
     assert svc_pr
 
 
-@pytest.mark.parametrize('config_path', ['src/aml_scripts/.aml_config/config.json'])
-def test_ws_init(config_path):
-    # set env variables
-    set_env_vars(config_path)
-
+@pytest.mark.cicd
+@pytest.mark.local
+def test_ws_init():
     # perform service principal authentication
     svc_pr = get_svc_pr()
 
